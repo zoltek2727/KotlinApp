ce81713a1186812f282774b86cbf78f5d79236e7
Sebastian <zoltek2727@wp.pl>
2018-05-19 02:26:10 +0200

    Nowe przyciski oraz przykładowe funkcje.

--- app/src/main/java/com/example/sebastian/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/sebastian/kotlinapp/MainActivity.kt
@@ -2,12 +2,26 @@ package com.example.sebastian.kotlinapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    fun reverseText(a:String):String {
+        return a.reversed();
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnConfirm.setOnClickListener {
+            Toast.makeText(this, etText.text, Toast.LENGTH_SHORT).show()
+        }
+
+        btnReverse.setOnClickListener {
+            Toast.makeText(this,reverseText(etText.text.toString()), Toast.LENGTH_LONG).show();
+        }
     }
 }
 //Branch test
